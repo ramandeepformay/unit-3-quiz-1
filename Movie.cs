a0494e47a7e222c2dd6413ed8a26deda1f9503f6
Ramandeep Formay <ramandeepformay@gmail.com>
2020-10-18 18:56:11 -0600

    Movie Rater

---
+++ Movie.cs
@@ -0,0 +1,44 @@
+using System;
+using System.Collections.Generic;
+namespace unit3
+{
+    public class Movie
+    {
+        public String Title { get; set; }
+        public String Director { get; set; }
+        public int Year{get; set;}
+        public int Id { get; set;}       
+        public List<int> Rating { get; set;}
+
+        public Movie(string title, string director, int year, int id, List<int> rating){
+            Title= title;
+            Director= director;
+            Year= year;
+            Id= id;
+            Rating = rating;
+        }
+        public void PrintDetails(){ 
+            System.Console.WriteLine($"Movie Title: {Title}");
+            System.Console.WriteLine($"Movie Director: {Director}");
+            System.Console.WriteLine($"Movie Title: {Year}");
+            
+        }
+
+        public void AddRating(int num){
+          System.Console.WriteLine($"{Title} rating is {num} \n");
+        }
+    
+        public float getAverageRating(){
+            int sum =0;
+            float avgSum=0f;
+            for(int i=0; i<Rating.Count; i++){
+                sum+=Rating[i];
+            }
+            if(Rating.Count>1){
+                avgSum = sum/(Rating.Count);
+            }
+            
+            return avgSum;
+        } 
+    }
+}
---
+++ Program.cs
@@ -0,0 +1,29 @@
+﻿using System.Data;
+using System;
+using System.Collections.Generic;
+
+namespace unit3
+{
+    class Program
+    {
+        public static void Main(string[] args)
+        { var list = new List<int>();
+        var i=0;
+            var m1 = new Movie("Interstellar","Christopher Nolan",2014,12, list);
+            while(i<3){
+                System.Console.WriteLine("Enter the Rating for the movie");
+                var userInput = Console.ReadLine();
+                System.Console.WriteLine("============ /n");
+                var intUserInput = Convert.ToInt32(userInput);
+                m1.AddRating(intUserInput);
+                list.Add(intUserInput);
+              i++;
+            }
+            System.Console.WriteLine("============");
+           m1.PrintDetails();
+           System.Console.WriteLine("============");
+           System.Console.WriteLine( $"{m1.Title}'s avergae Rating is {m1.getAverageRating()} ");
+        }
+    } 
+   
+}
